dd6aee71623b5f4f4c2c1805cf71dc1abf3de052
Robert Yamasaki <rhy2781@rit.edu>
2022-08-20 20:49:22 -0400

    formatted api request method from python

---
+++ api/pokemon.py
@@ -0,0 +1,36 @@
+import json
+
+import requests
+
+
+def request_and_process_pokemon_data(s):
+    api_url = "https://pokeapi.co/api/v2/pokemon/" + s + "/"
+    response = requests.get(api_url)
+    res = response.json()
+
+    # remove any unwanted data that is too complicated for us to process
+    del res["is_default"]
+    del res["order"]
+    del res["game_indices"]
+    del res["past_types"]
+    return res
+
+
+def main():
+
+    # this will store the entire list of pokemon that we query for
+    pokedex = dict()
+
+    # this will contain the information that we will store about the pokemon
+    data = request_and_process_pokemon_data("bulbasaur")
+    pokedex["bulbasaur"] = data
+
+    # file management
+    f = open("localDatabase.json", "w")
+    dump = json.dumps(pokedex)
+    f.write(dump)
+    f.close()
+
+
+if __name__ == "__main__":
+    main()
